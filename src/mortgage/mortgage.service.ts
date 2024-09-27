d81c605cb229c77626dc90933493967d12491c82
Bravolakmedia <olakunlebravo@gmail.com>
2024-09-27 21:36:32 +0100

    Running npm install for postinstall and npx prisma generate to make vercel deployment work perfectly

--- src/mortgage/mortgage.service.ts
+++ src/mortgage/mortgage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '..prisma.service';
+import { PrismaService } from '../database/prisma.service';
 import { CreateMortgageRequestDto } from './dto/create-mortgage-request.dto';
 import * as nodemailer from 'nodemailer';
 
