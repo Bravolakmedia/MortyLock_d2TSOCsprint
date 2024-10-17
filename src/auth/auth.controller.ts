6d2287d9f0fbf8594cd556cc590aac920a436c3f
Bravolakmedia <olakunlebravo@gmail.com>
2024-10-17 12:56:09 +0100

    Modifying the payload

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 // auth/auth.controller.ts
-import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto'; // Assuming you create a Login DTO
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
@@ -24,11 +24,7 @@ export class AuthController {
   @ApiOperation({ summary: 'User login' })
   @ApiResponse({ status: 200, description: 'User logged in' })
   async login(@Body() loginDto: LoginDto) {
-    const user = await this.authService.validateUser(loginDto.email, loginDto.password); // Pass both email and password
-  if (!user) {
-    throw new UnauthorizedException(); // Handle invalid login
-  }
-  return this.authService.login(user); // Now pass the user to the login method
+    const { email, password } = loginDto;
+  return this.authService.login(email, password);
   }
 }
-
