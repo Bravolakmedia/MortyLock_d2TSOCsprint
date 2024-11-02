1bab0299a8f8550869b950f0624c2a44f9dc6315
Bravolakmedia <olakunlebravo@gmail.com>
2024-11-02 18:23:36 +0100

    Fixing Cors

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 require('dotenv').config();
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
     origin: 'https://morty-lock-d2-tso-csprint-flax.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe())
   const config = new DocumentBuilder()
